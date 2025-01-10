e557ea298e47d778e5b0f6f0015439143c645298
darialgt <matchenkodasha@gmail.com>
2025-01-10 16:20:34 +0200

    Made player movement

---
+++ Assets/_Scripts/Managers/InputManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    public static Vector2 Movement {  get; private set; }
+
+    private PlayerInput _playerInput;
+    private InputAction _moveAction;
+
+    private void Awake()
+    {
+        _playerInput = GetComponent<PlayerInput>();
+
+        _moveAction = _playerInput.actions["Move"];
+    }
+
+    private void Update()
+    {
+        Movement = _moveAction.ReadValue<Vector2>();
+    }
+}
