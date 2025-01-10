c33f5d3cf3733ec94dccf3791d01a8d9c086a453
shatyloartem <shatyloartem@gmail.com>
2025-01-10 17:31:44 +0200

    Added player

---
+++ Assets/_Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+
+    private Vector2 _movementDirection;
+
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        _movementDirection.Set(InputManager.Movement.x, InputManager.Movement.y);
+
+        rb.velocity = _movementDirection * moveSpeed;
+    }
+}
