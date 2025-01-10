8ca990a83fdeb1a8c6b9cdfae12ee34b68453c96
darialgt <matchenkodasha@gmail.com>
2025-01-10 18:37:43 +0200

    Grenade counter

--- Assets/_Scripts/Player/ObjectThrow.cs
+++ Assets/_Scripts/Player/ObjectThrow.cs
@@ -1,11 +1,18 @@
 using DG.Tweening;
+using TMPro;
 using UnityEngine;
 
 public class ObjectThrow : MonoBehaviour
 {
     [SerializeField] private GameObject _granadePrefab;
+    [SerializeField] private TMP_Text _grenadeCounterText;
 
     private int _granadeCounter = 5;
+
+    private void Awake()
+    {
+        UpdateCounter();
+    }
 
     private void Update()
     {
@@ -20,7 +27,14 @@ public class ObjectThrow : MonoBehaviour
             Vector2 cursorPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             GameObject granade = Instantiate(_granadePrefab, transform.position, Quaternion.identity);
             granade.transform.DOMove(cursorPosition, .35f);
+
             _granadeCounter--;
+            UpdateCounter();
         }
+    }
+
+    private void UpdateCounter()
+    {
+        _grenadeCounterText.text = _granadeCounter.ToString();
     }
 }
