6b1c8e86eae61e3ef745eeb9ef8954ca291366c7
darialgt <matchenkodasha@gmail.com>
2025-01-10 21:59:14 +0200

    Locker hiding

---
+++ Assets/_Scripts/Player/LockerHider.cs
@@ -0,0 +1,57 @@
+using DG.Tweening;
+using UnityEngine;
+
+public class LockerHider : MonoBehaviour
+{
+    private PlayerMovement _playerMovement;
+    private BoxCollider2D _boxCollider;
+    private SpriteRenderer _spriteRenderer;
+    
+    private bool _isHidden;
+    private bool _isLockerInRange;
+    private Transform _locker;
+
+    private void Awake()
+    {
+        _playerMovement = GetComponent<PlayerMovement>();
+        _boxCollider = GetComponent<BoxCollider2D>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && _isHidden)
+        {
+            _playerMovement.enabled = true;
+            _boxCollider.enabled = true;
+            _isHidden = false;
+
+            _spriteRenderer.sortingOrder = 3;
+        }
+        else if (Input.GetKeyDown(KeyCode.E) && _isLockerInRange)
+        {
+            _playerMovement.enabled = false;
+            _boxCollider.enabled = false;
+            _isHidden = true;
+
+            _spriteRenderer.sortingOrder = 1;
+
+            transform.DOMove(_locker.position, .3f);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Locker"))
+        {
+            _isLockerInRange = true;
+            _locker = collision.transform;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Locker"))
+            _isLockerInRange = false;
+    }
+}
