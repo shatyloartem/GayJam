dbf076c8b8f29aa77d885d5f84928c7a19a52f6c
shatyloartem <shatyloartem@gmail.com>
2025-01-12 21:16:13 +0200

    Started fixing bug

---
+++ Assets/_Scripts/Utilities/PlayerLayers.cs
@@ -0,0 +1,32 @@
+using _Scripts.Player;
+using UnityEngine;
+
+namespace _Scripts.Utilities
+{
+    public class PlayerLayers : MonoBehaviour
+    {
+        private SpriteRenderer[] _spriteRenderers;
+        private Collider2D _boxCollider;
+        
+        private void Awake()
+        {
+            _spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
+            _boxCollider = GetComponent<Collider2D>();
+        }
+
+        private void OnTriggerStay2D(Collider2D other)
+        {
+            if (!other.CompareTag("Player")) return;
+
+            Debug.Log($"{PlayerController.Collider.bounds.center.y} | {_boxCollider.bounds.max.y}");
+            
+            SetLayer(PlayerController.Collider.bounds.center.y > _boxCollider.bounds.max.y ? "TopLayer" : "Default");
+        }
+
+        private void SetLayer(string layer)
+        {
+            foreach (var spriteRenderer in _spriteRenderers)
+                spriteRenderer.sortingLayerName = layer;
+        }
+    }
+}
