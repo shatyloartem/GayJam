0be11db95f89547df41caca44696ebfae521b88b
shatyloartem <shatyloartem@gmail.com>
2025-01-12 18:41:13 +0200

    Made player animations

--- Assets/_Scripts/Player/PlayerMovement.cs
+++ Assets/_Scripts/Player/PlayerMovement.cs
@@ -1,10 +1,11 @@
-using System;
 using _Scripts.UI;
 using _Scripts.Utilities;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private static readonly int WalkState = Animator.StringToHash("WalkState");
+    
     [SerializeField] private float moveSpeed = 5f;
 
     private bool _isGamePaused;
@@ -12,11 +13,15 @@ public class PlayerMovement : MonoBehaviour
     private Vector2 _movementDirection;
 
     private Rigidbody2D rb;
+    private Animator _animator;
+    private SpriteRenderer _spriteRenderer;
     private StepSoundController _stepSoundController;
-
+    
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
         _stepSoundController = GetComponentInChildren<StepSoundController>();
         
         LoadPosition();
@@ -46,6 +51,8 @@ public class PlayerMovement : MonoBehaviour
         _movementDirection.Set(InputManager.Movement.x, InputManager.Movement.y);
 
         rb.velocity = _movementDirection * moveSpeed;
+        
+        UpdateAnimations();
 
         if (_movementDirection == Vector2.zero)
             _stepSoundController.SoundOff();
@@ -69,5 +76,28 @@ public class PlayerMovement : MonoBehaviour
             return;
         
         transform.position = savedPosition;
+    }
+
+    private void UpdateAnimations()
+    {
+        int state = 0;
+        if(InputManager.Movement.x == 0 && InputManager.Movement.y > 0)
+            state = 3;
+        else if (InputManager.Movement.x == 0 && InputManager.Movement.y < 0)
+            state = 2;
+        else switch (InputManager.Movement.x)
+        {
+            case > 0:
+                _spriteRenderer.flipX = false;
+                state = 1;
+                break;
+            case < 0:
+                _spriteRenderer.flipX = true;
+                state = 1;
+                break;
+        }
+
+        Debug.Log($"{InputManager.Movement.x} | {InputManager.Movement.y} | {state}");
+        _animator.SetInteger(WalkState, state);
     }
 }
